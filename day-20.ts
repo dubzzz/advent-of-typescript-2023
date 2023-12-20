1d267a168c48ea2c4b6231a7bb113715710cf2d6
Nicolas DUBIEN <github@dubien.org>
2023-12-20 08:18:46 +0000

    Day 20

--- day-20.ts
+++ day-20.ts
@@ -1,7 +1,133 @@
 import { Expect, Equal } from "type-testing";
 
 // Subject:
+type Letters = {
+  A: ["█▀█ ", "█▀█ ", "▀ ▀ "];
+  B: ["█▀▄ ", "█▀▄ ", "▀▀  "];
+  C: ["█▀▀ ", "█ ░░", "▀▀▀ "];
+  E: ["█▀▀ ", "█▀▀ ", "▀▀▀ "];
+  H: ["█ █ ", "█▀█ ", "▀ ▀ "];
+  I: ["█ ", "█ ", "▀ "];
+  M: ["█▄░▄█ ", "█ ▀ █ ", "▀ ░░▀ "];
+  N: ["█▄░█ ", "█ ▀█ ", "▀ ░▀ "];
+  P: ["█▀█ ", "█▀▀ ", "▀ ░░"];
+  R: ["█▀█ ", "██▀ ", "▀ ▀ "];
+  S: ["█▀▀ ", "▀▀█ ", "▀▀▀ "];
+  T: ["▀█▀ ", "░█ ░", "░▀ ░"];
+  Y: ["█ █ ", "▀█▀ ", "░▀ ░"];
+  W: ["█ ░░█ ", "█▄▀▄█ ", "▀ ░ ▀ "];
+  " ": ["░", "░", "░"];
+  ":": ["#", "░", "#"];
+  "*": ["░", "#", "░"];
+};
+// type ToAsciiArt = unknown;
 
 // My solution:
+type JoinLetters<
+  TLetters extends [string, string, string][],
+  TAcc extends [string, string, string] = ["", "", ""],
+> = TLetters extends [
+  [infer S1 extends string, infer S2 extends string, infer S3 extends string],
+  ...infer TRest extends [string, string, string][],
+]
+  ? JoinLetters<
+      TRest,
+      [`${TAcc[0]}${S1}`, `${TAcc[1]}${S2}`, `${TAcc[2]}${S3}`]
+    >
+  : TAcc;
+type JoinLetterLines<
+  TLines extends [string, string, string][],
+  TAcc extends string[] = [],
+> = TLines extends [
+  infer TLine extends [string, string, string],
+  ...infer TRest extends [string, string, string][],
+]
+  ? JoinLetterLines<TRest, [...TAcc, ...TLine]>
+  : TAcc;
+type ToUpper<
+  S extends string,
+  TAcc extends string = "",
+> = S extends `${infer C}${infer Rest}`
+  ? ToUpper<Rest, `${TAcc}${Uppercase<C>}`>
+  : TAcc;
+type LineToLetters<
+  S extends string,
+  TAcc extends [string, string, string][] = [],
+> = S extends `${infer C extends keyof Letters}${infer Rest}`
+  ? LineToLetters<Rest, [...TAcc, Letters[C]]>
+  : TAcc;
+type StringToLines<
+  S extends string,
+  TAcc extends string[] = [],
+> = S extends `${infer TStart}\n${infer TEnd}`
+  ? StringToLines<TEnd, [...TAcc, TStart]>
+  : [...TAcc, S];
+type LinesToLetterLines<
+  TLines extends string[],
+  TAcc extends [string, string, string][] = [],
+> = TLines extends [infer TLine extends string, ...infer TRest extends string[]]
+  ? LinesToLetterLines<
+      TRest,
+      [...TAcc, JoinLetters<LineToLetters<ToUpper<TLine>>>]
+    >
+  : TAcc;
+type ToAsciiArt<S extends string> = JoinLetterLines<
+  LinesToLetterLines<StringToLines<S>>
+>;
 
 // Tests:
+type test_0_actual = ToAsciiArt<"   * : * Merry * : *   \n  Christmas  ">;
+//   ^?
+type test_0_expected = [
+  "░░░░░#░░░█▄░▄█ █▀▀ █▀█ █▀█ █ █ ░░░#░░░░░",
+  "░░░#░░░#░█ ▀ █ █▀▀ ██▀ ██▀ ▀█▀ ░#░░░#░░░",
+  "░░░░░#░░░▀ ░░▀ ▀▀▀ ▀ ▀ ▀ ▀ ░▀ ░░░░#░░░░░",
+  "░░█▀▀ █ █ █▀█ █ █▀▀ ▀█▀ █▄░▄█ █▀█ █▀▀ ░░",
+  "░░█ ░░█▀█ ██▀ █ ▀▀█ ░█ ░█ ▀ █ █▀█ ▀▀█ ░░",
+  "░░▀▀▀ ▀ ▀ ▀ ▀ ▀ ▀▀▀ ░▀ ░▀ ░░▀ ▀ ▀ ▀▀▀ ░░",
+];
+type test_0 = Expect<Equal<test_0_actual, test_0_expected>>;
+
+type test_1_actual = ToAsciiArt<"  Happy new  \n  * : * : * Year * : * : *  ">;
+//   ^?
+type test_1_expected = [
+  "░░█ █ █▀█ █▀█ █▀█ █ █ ░█▄░█ █▀▀ █ ░░█ ░░",
+  "░░█▀█ █▀█ █▀▀ █▀▀ ▀█▀ ░█ ▀█ █▀▀ █▄▀▄█ ░░",
+  "░░▀ ▀ ▀ ▀ ▀ ░░▀ ░░░▀ ░░▀ ░▀ ▀▀▀ ▀ ░ ▀ ░░",
+  "░░░░#░░░#░░░█ █ █▀▀ █▀█ █▀█ ░░░#░░░#░░░░",
+  "░░#░░░#░░░#░▀█▀ █▀▀ █▀█ ██▀ ░#░░░#░░░#░░",
+  "░░░░#░░░#░░░░▀ ░▀▀▀ ▀ ▀ ▀ ▀ ░░░#░░░#░░░░",
+];
+type test_1 = Expect<Equal<test_1_actual, test_1_expected>>;
+
+type test_2_actual =
+  ToAsciiArt<"  * : * : * : * : * : * \n  Trash  \n  * : * : * : * : * : * ">;
+//   ^?
+type test_2_expected = [
+  "░░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░▀█▀ █▀█ █▀█ █▀▀ █ █ ░░",
+  "░░░█ ░██▀ █▀█ ▀▀█ █▀█ ░░",
+  "░░░▀ ░▀ ▀ ▀ ▀ ▀▀▀ ▀ ▀ ░░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░",
+];
+type test_2 = Expect<Equal<test_2_actual, test_2_expected>>;
+
+type test_3_actual =
+  ToAsciiArt<"  : * : * : * : * : * : * : \n  Ecyrbe  \n  : * : * : * : * : * : * : ">;
+//   ^?
+type test_3_expected = [
+  "░░#░░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░█▀▀ █▀▀ █ █ █▀█ █▀▄ █▀▀ ░░",
+  "░░█▀▀ █ ░░▀█▀ ██▀ █▀▄ █▀▀ ░░",
+  "░░▀▀▀ ▀▀▀ ░▀ ░▀ ▀ ▀▀  ▀▀▀ ░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░░░#░",
+];
+type test_3 = Expect<Equal<test_3_actual, test_3_expected>>;
