b99e629c97f947d45e45cb234b6025ace05bae2b
Nicolas DUBIEN <github@dubien.org>
2023-12-25 11:15:42 +0000

    Day 23 start

--- day-23.ts
+++ day-23.ts
@@ -1,7 +1,330 @@
 import { Expect, Equal } from "type-testing";
 
 // Subject:
+type Connect4Chips = "🔴" | "🟡";
+type Connect4Cell = Connect4Chips | "  ";
+type Connect4State = "🔴" | "🟡" | "🔴 Won" | "🟡 Won" | "Draw";
+type EmptyBoard = [
+  ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+  ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+  ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+  ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+  ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+  ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+];
+type NewGame = { board: EmptyBoard; state: "🟡" };
+// type Connect4 = unknown;
 
 // My solution:
+type Board = Connect4Cell[][];
+type Game = { board: Board; state: Connect4State };
+type ReadColumn<
+  TBoard extends Board,
+  TColumnIndex extends number,
+  TReadData extends Board[number] = [],
+  TAcc extends string = "",
+> = TReadData["length"] extends TBoard["length"]
+  ? TAcc
+  : ReadColumn<
+      TBoard,
+      TColumnIndex,
+      [...TReadData, TBoard[TReadData["length"]][TColumnIndex]],
+      `${TAcc}${TBoard[TReadData["length"]][TColumnIndex] extends "  "
+        ? " "
+        : TBoard[TReadData["length"]][TColumnIndex] extends "🔴"
+          ? "R"
+          : "Y"}`
+    >;
+type ReadColumns<
+  TBoard extends Board,
+  TAcc extends string[] = [],
+> = TAcc["length"] extends TBoard[0]["length"]
+  ? TAcc
+  : ReadColumns<TBoard, [...TAcc, ReadColumn<TBoard, TAcc["length"]>]>;
+type ColumnToBoard<
+  TColumn extends string,
+  TAcc extends any[] = [],
+> = TColumn extends `${infer TCell}${infer TRest}`
+  ? ColumnToBoard<
+      TRest,
+      [...TAcc, TCell extends "R" ? "🔴" : TCell extends "Y" ? "🟡" : "  "]
+    >
+  : TAcc;
+type ColumnsToBoard<
+  TColumns extends string[],
+  TAcc extends Board = [],
+> = TAcc["length"] extends TColumns[0]["length"]
+  ? TAcc
+  : ColumnsToBoard<
+      TColumns,
+      [...TAcc, ColumnToBoard<TColumns[TAcc["length"]]>]
+    >;
+
+type PPP = ReadColumns<test_move2_expected["board"]>;
+type QQQ = PPP["0"];
+type WWW = ColumnToBoard<QQQ>;
+
+/*type PlayMoveColumn<
+  TBoardColumn extends Board[number],
+  TChip extends Connect4Chips,
+  TNewBoardColumn extends Board[number] = [],
+> = TNewBoardColumn["length"] extends TBoardColumn["length"]
+  ? TNewBoardColumn
+  : TBoardColumn[TNewBoardColumn["length"]] extends "  "
+    ? TBoardColumn[[...TNewBoardColumn, []]["length"]] extends Connect4Chips // followed by another chip
+      ? PlayMoveColumn<TBoardColumn, TChip, [...TNewBoardColumn, TChip]>
+      : [...TNewBoardColumn, []]["length"] extends TBoardColumn["length"] // first chip
+        ? PlayMoveColumn<TBoardColumn, TChip, [...TNewBoardColumn, TChip]>
+        : PlayMoveColumn<
+            TBoardColumn,
+            TChip,
+            [...TNewBoardColumn, TBoardColumn[TNewBoardColumn["length"]]]
+          >
+    : PlayMoveColumn<
+        TBoardColumn,
+        TChip,
+        [...TNewBoardColumn, TBoardColumn[TNewBoardColumn["length"]]]
+      >;
+type PlayMove<
+  TTransposedBoard extends Board,
+  TChip extends Connect4Chips,
+  TColumnIndex extends number,
+  TNewTransposedBoard extends Board = [],
+> = TNewTransposedBoard["length"] extends TTransposedBoard["length"]
+  ? TNewTransposedBoard
+  : TNewTransposedBoard["length"] extends TColumnIndex
+    ? PlayMove<
+        TTransposedBoard,
+        TChip,
+        TColumnIndex,
+        [
+          ...TNewTransposedBoard,
+          PlayMoveColumn<TTransposedBoard[TColumnIndex], TChip>,
+        ]
+      >
+    : PlayMove<
+        TTransposedBoard,
+        TChip,
+        TColumnIndex,
+        [...TNewTransposedBoard, TTransposedBoard[TColumnIndex]]
+      >;*/
+type Connect4<TGame extends Game, TColumn extends number> = TGame extends {
+  board: infer TBoard extends Board;
+  state: infer TChip extends Connect4Chips;
+}
+  ? unknown /*{
+      board: Transpose<PlayMove<Transpose<TBoard>, TChip, TColumn>>;
+      state: TChip extends "🔴" ? "🟡" : "🔴";
+    }*/
+  : TGame;
 
 // Tests:
+type test_move1_actual = Connect4<NewGame, 0>;
+//   ^?
+type test_move1_expected = {
+  board: [
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+  ];
+  state: "🔴";
+};
+type test_move1 = Expect<Equal<test_move1_actual, test_move1_expected>>;
+
+type test_move2_actual = Connect4<test_move1_actual, 0>;
+//   ^?
+type test_move2_expected = {
+  board: [
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🔴", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+  ];
+  state: "🟡";
+};
+type test_move2 = Expect<Equal<test_move2_actual, test_move2_expected>>;
+
+type test_move3_actual = Connect4<test_move2_actual, 0>;
+//   ^?
+type test_move3_expected = {
+  board: [
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🔴", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+  ];
+  state: "🔴";
+};
+type test_move3 = Expect<Equal<test_move3_actual, test_move3_expected>>;
+
+type test_move4_actual = Connect4<test_move3_actual, 1>;
+//   ^?
+type test_move4_expected = {
+  board: [
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🔴", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "🔴", "  ", "  ", "  ", "  ", "  "],
+  ];
+  state: "🟡";
+};
+type test_move4 = Expect<Equal<test_move4_actual, test_move4_expected>>;
+
+type test_move5_actual = Connect4<test_move4_actual, 2>;
+//   ^?
+type test_move5_expected = {
+  board: [
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🔴", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "🔴", "🟡", "  ", "  ", "  ", "  "],
+  ];
+  state: "🔴";
+};
+type test_move5 = Expect<Equal<test_move5_actual, test_move5_expected>>;
+
+type test_move6_actual = Connect4<test_move5_actual, 1>;
+//   ^?
+type test_move6_expected = {
+  board: [
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🔴", "🔴", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "🔴", "🟡", "  ", "  ", "  ", "  "],
+  ];
+  state: "🟡";
+};
+type test_move6 = Expect<Equal<test_move6_actual, test_move6_expected>>;
+
+type test_red_win_actual = Connect4<
+  {
+    board: [
+      ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+      ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+      ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+      ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+      ["🔴", "🔴", "🔴", "  ", "  ", "  ", "  "],
+      ["🟡", "🔴", "🟡", "🟡", "  ", "  ", "  "],
+    ];
+    state: "🔴";
+  },
+  3
+>;
+
+type test_red_win_expected = {
+  board: [
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🔴", "🔴", "🔴", "🔴", "  ", "  ", "  "],
+    ["🟡", "🔴", "🟡", "🟡", "  ", "  ", "  "],
+  ];
+  state: "🔴 Won";
+};
+
+type test_red_win = Expect<Equal<test_red_win_actual, test_red_win_expected>>;
+
+type test_yellow_win_actual = Connect4<
+  {
+    board: [
+      ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+      ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+      ["🔴", "  ", "  ", "  ", "  ", "  ", "  "],
+      ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+      ["🔴", "  ", "🔴", "🔴", "  ", "  ", "  "],
+      ["🟡", "  ", "🟡", "🟡", "  ", "  ", "  "],
+    ];
+    state: "🟡";
+  },
+  1
+>;
+
+type test_yellow_win_expected = {
+  board: [
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🔴", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🟡", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["🔴", "  ", "🔴", "🔴", "  ", "  ", "  "],
+    ["🟡", "🟡", "🟡", "🟡", "  ", "  ", "  "],
+  ];
+  state: "🟡 Won";
+};
+
+type test_yellow_win = Expect<
+  Equal<test_yellow_win_actual, test_yellow_win_expected>
+>;
+
+type test_diagonal_yellow_win_actual = Connect4<
+  {
+    board: [
+      ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+      ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+      ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+      ["  ", "  ", "🟡", "🔴", "  ", "  ", "  "],
+      ["🔴", "🟡", "🔴", "🔴", "  ", "  ", "  "],
+      ["🟡", "🔴", "🟡", "🟡", "  ", "  ", "  "],
+    ];
+    state: "🟡";
+  },
+  3
+>;
+
+type test_diagonal_yellow_win_expected = {
+  board: [
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "🟡", "  ", "  ", "  "],
+    ["  ", "  ", "🟡", "🔴", "  ", "  ", "  "],
+    ["🔴", "🟡", "🔴", "🔴", "  ", "  ", "  "],
+    ["🟡", "🔴", "🟡", "🟡", "  ", "  ", "  "],
+  ];
+  state: "🟡 Won";
+};
+
+type test_diagonal_yellow_win = Expect<
+  Equal<test_diagonal_yellow_win_actual, test_diagonal_yellow_win_expected>
+>;
+
+type test_draw_actual = Connect4<
+  {
+    board: [
+      ["🟡", "🔴", "🔴", "🟡", "🟡", "🔴", "  "],
+      ["🔴", "🟡", "🟡", "🔴", "🔴", "🟡", "🔴"],
+      ["🟡", "🔴", "🔴", "🟡", "🟡", "🔴", "🟡"],
+      ["🔴", "🟡", "🟡", "🔴", "🔴", "🟡", "🔴"],
+      ["🟡", "🔴", "🔴", "🟡", "🟡", "🔴", "🟡"],
+      ["🔴", "🟡", "🟡", "🔴", "🔴", "🟡", "🔴"],
+    ];
+    state: "🟡";
+  },
+  6
+>;
+
+type test_draw_expected = {
+  board: [
+    ["🟡", "🔴", "🔴", "🟡", "🟡", "🔴", "🟡"],
+    ["🔴", "🟡", "🟡", "🔴", "🔴", "🟡", "🔴"],
+    ["🟡", "🔴", "🔴", "🟡", "🟡", "🔴", "🟡"],
+    ["🔴", "🟡", "🟡", "🔴", "🔴", "🟡", "🔴"],
+    ["🟡", "🔴", "🔴", "🟡", "🟡", "🔴", "🟡"],
+    ["🔴", "🟡", "🟡", "🔴", "🔴", "🟡", "🔴"],
+  ];
+  state: "Draw";
+};
+
+type test_draw = Expect<Equal<test_draw_actual, test_draw_expected>>;
